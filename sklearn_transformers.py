35378689830fc63323dca477bac49e1d44358141
Felipe Ribeiro Tanso <felipetanso@gmail.com>
2020-12-05 16:00:15 -0300

    Add files via upload

---
+++ sklearn_transformers.py
@@ -0,0 +1,27 @@
+from sklearn.base import BaseEstimator, TransformerMixin
+
+
+# All sklearn Transforms must have the `transform` and `fit` methods
+class DropColumns(BaseEstimator, TransformerMixin):
+    def __init__(self, columns):
+        self.columns = columns
+
+    def fit(self, X, y=None):
+        return self
+
+    def transform(self, X):
+        # Primeiro realizamos a cópia do dataframe 'X' de entrada
+        data = X.copy()
+        # Retornamos um novo dataframe sem as colunas indesejadas
+        return data.drop(labels=self.columns, axis='columns')
+
+class TrocaValor(BaseEstimator, TransformerMixin):
+    def fit(self, X, y=None):
+        return self
+
+    def transform(self, X):
+        # Primeiro realizamos a cópia do dataframe 'X' de entrada
+        data = X.copy()
+        data['koi_pdisposition'] = data.apply(lambda x = 1: koi_pdisposition == 'FALSE POSITIVE' else 2, axis=1)
+        # Retornamos um novo dataframe
+        return data
